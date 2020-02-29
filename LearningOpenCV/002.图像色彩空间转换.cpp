5be9eb47ae872660ae4ea1301374c3fbc57bc84b
Smartuil <563412673@qq.com>
2020-02-29 20:47:31 +0800

    add new

---
+++ LearningOpenCV/002.图像色彩空间转换.cpp
@@ -0,0 +1,22 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int artc, char** argv) {
+	Mat src = imread("D:/images/test.png");
+	if (src.empty()) {
+		printf("could not load image...\n");
+		return -1;
+	}
+	namedWindow("input", WINDOW_AUTOSIZE);
+	imshow("input", src);
+
+	Mat gray;
+	cvtColor(src, gray, COLOR_BGR2GRAY);
+	imwrite("D:/gray.png", gray);
+
+	waitKey(0);
+	return 0;
+}
